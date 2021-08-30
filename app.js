bc766fccc649a181581c52ef0383451acb2d9e36
tomit4 <mosssap@gmail.com>
2021-08-30 09:34:58 -0700

    added basic CRUD functionality

--- app.js
+++ app.js
@@ -2,19 +2,62 @@ const express = require('express')
 const app = express()
 const knex = require('./db/knex')
 
-// app.use(express.urlencoded({ extended:false }))
-// app.use(express.json())
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(express.json())
 
+// GET request
 app.get('/', (req, res) => {
-    
-    // Insert knex queries here
-    knex('users').where({username: 'Caspar'}).select('email').then((results) => {
+
+    // same as SELECT * FROM users;
+    knex.select().from('users').then((results) => {
         res.send(results)
     })
-
 })
 
+// POST request
+app.post('/', (req, res) => {
+    knex('users').insert({
+            id: req.body.id,
+            username: req.body.username,
+            email: req.body.email
+        })
+        .then(() => {
+            knex.select().from('users')
+                .then((results) => {
+                    res.send(results)
+                })
+        })
+})
+
+// PUT request
+app.put('/', (req, res) => {
+    knex('users').where('id', req.body.id).update({
+            username: req.body.username,
+            email: req.body.email
+        })
+        .then(() => {
+            knex.select().from('users')
+                .then((results) => {
+                    res.send(results)
+                })
+        })
+})
+
+// DELETE request
+app.delete('/', (req, res) => {
+    knex('users').where('id', req.body.id).del()
+        .then(() => {
+            knex.select().from('users')
+                .then((results) => {
+                    res.send(results)
+                })
+        })
+})
+
+
 app.listen(3000, (err) => {
-    if(err) console.log(err)
+    if (err) console.log(err)
     console.log('Listening on port: 3000')
 })
